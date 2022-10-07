9fb2cae2e54fbb85c09b58ea546ac200c41cba77
Kelley Mueller <111308701+KLMueller@users.noreply.github.com>
2022-10-07 11:56:51 -0500

    Area of a triangle calculator
    Beginner exercise from class 6

---
+++ Area of Triangle/main.js
@@ -0,0 +1,14 @@
+// <!-- Aim: Create a function that calculates the area of a triangle given the base and height as arguments.
+
+function Area() {
+  var side1 = parseInt(document.getElementById("side1").value);
+  var side2 = parseInt(document.getElementById("side2").value);
+  var side3 = parseInt(document.getElementById("side3").value);
+
+  console.log(typeof side1);
+  var s = (side1 + side2 + side3) / 2;
+
+  var area = Math.sqrt(s * ((s - side1) * (s - side2) * (s - side3)));
+
+  document.getElementById("display").innerHTML = area;
+}
